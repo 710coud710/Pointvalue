5b0178e5f9f10c03c91e47f565efd6f2e4e11cfb
Duoc <157684306+710coud710@users.noreply.github.com>
2024-10-05 09:07:11 +0700

    Add files via upload

--- apprun.py
+++ apprun.py
@@ -253,4 +253,4 @@ def download_file():
     return send_file(limited_filepath, as_attachment=True, download_name=limited_filepath)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
